c770998fae01fbf1e21b03934b0c24643204110b
Baek2back <dumbuk12@naver.com>
2022-02-06 18:06:31 +0900

    Feat: 사용할 웹 폰트에 preload를 설정한다

---
+++ pages/_document.tsx
@@ -0,0 +1,94 @@
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
+
+  render() {
+    return (
+      <Html lang="ko">
+        <Head>
+          <link
+            href="/fonts/Gilroy-ExtraBold.woff2"
+            rel="preload"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="/fonts/Gilroy-ExtraBold.woff"
+            rel="preload"
+            as="font"
+            type="font/woff"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="/fonts/SpoqaHanSansNeo-Light.woff2"
+            rel="preload"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="/fonts/SpoqaHanSansNeo-Light.woff"
+            rel="preload"
+            as="font"
+            type="font/woff"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="/fonts/SpoqaHanSansNeo-Regular.woff2"
+            rel="preload"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="/fonts/SpoqaHanSansNeo-Regular.woff"
+            rel="preload"
+            as="font"
+            type="font/woff"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="/fonts/SpoqaHanSansNeo-Medium.woff2"
+            rel="preload"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="/fonts/SpoqaHanSansNeo-Medium.woff"
+            rel="preload"
+            as="font"
+            type="font/woff"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="/fonts/SpoqaHanSansNeo-Bold.woff2"
+            rel="preload"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="/fonts/SpoqaHanSansNeo-Bold.woff"
+            rel="preload"
+            as="font"
+            type="font/woff"
+            crossOrigin="anonymous"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
