157e7098ca3fa87cf1529e5c6f307c3ed65f0208
HaJunRyu <hjr4557@naver.com>
2022-02-09 02:22:51 +0900

    Feat: 프로젝트 전역에 적용 될 Layout 컴포넌트에서 사용 할 Header 정의
    Changes to be committed:
    new file:   src/assets/svg/mash-up-logo.svg
    new file:   src/components/common/Header/Header.styled.ts
    new file:   src/components/common/Header/Header.tsx

---
+++ src/components/common/Header/Header.tsx
@@ -0,0 +1,22 @@
+import { LinkTo, MainNavigation } from '@/components';
+import MashUpLogo from '@/assets/svg/mash-up-logo.svg';
+import { HOME_PAGE } from '@/consts';
+import * as Styled from './Header.styled';
+
+const Header = () => {
+  return (
+    <Styled.Header>
+      <Styled.HeaderInner>
+        <LinkTo href={HOME_PAGE}>
+          <Styled.Heading aria-label="Mash Up Recruit">
+            <MashUpLogo />
+            <span>Mash-Up Recruit</span>
+          </Styled.Heading>
+        </LinkTo>
+        <MainNavigation />
+      </Styled.HeaderInner>
+    </Styled.Header>
+  );
+};
+
+export default Header;
