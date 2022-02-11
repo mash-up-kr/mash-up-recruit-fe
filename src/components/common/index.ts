00b59edfd04f470b1186392e709aefa61ca31d85
HaJunRyu <hjr4557@naver.com>
2022-02-12 06:35:57 +0900

    Feat: Portal 컴포넌트 정의
    Changes to be committed:
    modified:   package.json
    new file:   src/components/common/Portal/Portal.component.tsx
    modified:   src/components/common/index.ts
    modified:   yarn.lock

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -2,3 +2,4 @@ export { default as LinkTo } from './LinkTo/LinkTo';
 export { default as MainNavigation } from './MainNavigation/MainNavigation';
 export { default as Header } from './Header/Header';
 export { default as Layout } from './Layout/Layout';
+export { default as Portal } from './Portal/Portal.component';
