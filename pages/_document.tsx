f7b8c60fbec3e865ecf3d86f75be0bae4fbbbcfb
HaJunRyu <hjr4557@naver.com>
2022-02-12 06:34:44 +0900

    Feat: Portal을 사용하기 위한 modal-root 정적 Node 추가
    Changes to be committed:
    modified:   pages/_document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -49,6 +49,7 @@ class MyDocument extends Document {
         </Head>
         <body>
           <Main />
+          <div id="modal-root" />
           <NextScript />
         </body>
       </Html>
