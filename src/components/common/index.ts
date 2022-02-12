1584d6f5a0920f45c40dfae18eafe9f40ad4d119
HaJunRyu <hjr4557@naver.com>
2022-02-12 13:05:00 +0900

    Refactor: 컨벤션에 따른 컴포넌트 파일명 변경
    Changes to be committed:
    renamed:    src/components/common/Header/Header.tsx -> src/components/common/Header/Header.component.tsx
    renamed:    src/components/common/Layout/Layout.tsx -> src/components/common/Layout/Layout.component.tsx
    renamed:    src/components/common/LinkTo/LinkTo.tsx -> src/components/common/LinkTo/LinkTo.component.tsx
    renamed:    src/components/common/MainNavigation/MainNavigation.tsx -> src/components/common/MainNavigation/MainNavigation.component.tsx
    modified:   src/components/common/index.ts

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,7 +1,7 @@
-export { default as LinkTo } from './LinkTo/LinkTo';
-export { default as MainNavigation } from './MainNavigation/MainNavigation';
-export { default as Header } from './Header/Header';
-export { default as Layout } from './Layout/Layout';
+export { default as LinkTo } from './LinkTo/LinkTo.component';
+export { default as MainNavigation } from './MainNavigation/MainNavigation.component';
+export { default as Header } from './Header/Header.component';
+export { default as Layout } from './Layout/Layout.component';
 export { default as Portal } from './Portal/Portal.component';
 export { default as Modal } from './Modal/Modal.component';
 export { default as LoginModalDialog } from './LoginModalDialog/LoginModalDialog.component';
