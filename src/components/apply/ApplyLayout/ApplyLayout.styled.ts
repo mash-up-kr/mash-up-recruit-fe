ade89aed0d1c5fc483d77cb9308ceb15bfc02354
HaJunRyu <hjr4557@naver.com>
2022-02-16 21:50:07 +0900

    Feat: ApplyLayout 상하 padding 반응형 작업 (모바일 뷰)
    Changes to be committed:
    modified:   src/components/apply/ApplyLayout/ApplyLayout.styled.ts

--- src/components/apply/ApplyLayout/ApplyLayout.styled.ts
+++ src/components/apply/ApplyLayout/ApplyLayout.styled.ts
@@ -2,10 +2,16 @@ import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 
 export const Layout = styled.div`
-  width: 100%;
-  max-width: 82.4rem;
-  margin: 0 auto;
-  padding: 8.4rem 2rem 15.8rem;
+  ${({ theme }) => css`
+    width: 100%;
+    max-width: 82.4rem;
+    margin: 0 auto;
+    padding: 8.4rem 2rem 15.8rem;
+
+    @media (max-width: ${theme.breakPoint.mobile}) {
+      padding: 3.6rem 2rem 9.9rem;
+    }
+  `}
 `;
 
 export const ApplyHeading = styled.h2`
