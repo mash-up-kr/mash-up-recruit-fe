a2b32fa76702b4a14914a22883edd3a09cc53381
Baek2back <dumbuk12@naver.com>
2022-02-18 03:33:03 +0900

    Feat: next-auth를 API 서버와 연동한다

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,19 @@ import type { AppProps } from 'next/app';
 import { Global, ThemeProvider } from '@emotion/react';
 import { globalStyles, theme } from '@/styles';
 import { Layout } from '@/components';
+import { SessionProvider } from 'next-auth/react';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Global styles={globalStyles} />
-      <ThemeProvider theme={theme}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ThemeProvider>
+      <SessionProvider session={pageProps.session}>
+        <ThemeProvider theme={theme}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ThemeProvider>
+      </SessionProvider>
     </>
   );
 };
