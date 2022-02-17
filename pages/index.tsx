410a2c38886befb8cfdc031e2504db87146b6a0c
Baek2back <dumbuk12@naver.com>
2022-02-18 03:37:13 +0900

    Feat: 로그인 여부를 판별할 수 있는 임시 페이지를 구현한다

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,25 @@
 import type { NextPage } from 'next';
+import { useSession, signIn, signOut } from 'next-auth/react';
 
 const Home: NextPage = () => {
-  return <div />;
+  const { data } = useSession();
+
+  if (data) {
+    return (
+      <>
+        <div>로그인되었습니다.</div>
+        <button type="button" onClick={() => signOut()}>
+          로그아웃
+        </button>
+      </>
+    );
+  }
+
+  return (
+    <button type="button" onClick={() => signIn('google')}>
+      로그인
+    </button>
+  );
 };
 
 export default Home;
