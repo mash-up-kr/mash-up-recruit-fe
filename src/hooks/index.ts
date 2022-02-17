649770ffc8a866428973b6d438fd1a950b7783bf
HaJunRyu <hjr4557@naver.com>
2022-02-18 05:13:16 +0900

    Feat: 스크롤이 가장 최상단에 있는지 판단하여 상태값을 return해주는 hook 정의
    Changes to be committed:
    modified:   src/hooks/index.ts
    new file:   src/hooks/useWatchingIsScrollTop.ts

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,3 @@
-export { default as usePreventPageChange } from './useWarnIfUnsavedChanges';
+export { default as usePreventPageChange } from './usePreventPageChangeWithConfirmModal';
 export { default as useDetectViewPort } from './useDetectViewPort';
+export { default as useWatchingScrollIsTop } from './useWatchingIsScrollTop';
