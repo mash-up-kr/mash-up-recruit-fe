afd15c3d695520e51687f5d87b892100520947da
Baek2back <dumbuk12@naver.com>
2022-02-19 05:43:49 +0900

    Env: @svgr/webpack에서 svg에 대한 viewBox 제거 옵션 비활성화

--- next.config.js
+++ next.config.js
@@ -32,7 +32,25 @@ const nextConfig = {
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ['@svgr/webpack'],
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            svgoConfig: {
+              plugins: [
+                {
+                  name: 'preset-default',
+                  params: {
+                    overrides: {
+                      removeViewBox: false,
+                    },
+                  },
+                },
+              ],
+            },
+          },
+        },
+      ],
     });
 
     return config;
