d3430a4eeea6035fbe5cf6ffb2c2e0854aedb277
Baek2back <dumbuk12@naver.com>
2022-02-19 05:44:24 +0900

    Feat: 자주 묻는 질문 관련 라우팅 경로를 정의한다

--- src/constants/route.ts
+++ src/constants/route.ts
@@ -1,5 +1,13 @@
 export const HOME_PAGE = '/';
+
 export const FAQ_PAGE = '/faq';
+export const FAQ_FRONT_END_PAGE = '/faq/front-end';
+export const FAQ_DESIGN_PAGE = '/faq/design';
+export const FAQ_NODE_PAGE = '/faq/node';
+export const FAQ_SPRING_PAGE = '/faq/spring';
+export const FAQ_IOS_PAGE = '/faq/ios';
+export const FAQ_ANDROID_PAGE = '/faq/android';
+
 export const APPLY_FRONT_END_PAGE = '/apply/front-end';
 export const APPLY_DESIGN_PAGE = '/apply/design';
 export const APPLY_NODE_PAGE = '/apply/node';
