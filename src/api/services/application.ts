0444ff9a556daca7c6b6f0320127f3b7715afb2e
HaJunRyu <hjr4557@naver.com>
2022-02-20 07:31:40 +0900

    Feat: Application에 대한 service 정의
    Changes to be committed:
    new file:   src/api/services/application.ts
    modified:   src/api/services/index.ts

---
+++ src/api/services/application.ts
@@ -0,0 +1,100 @@
+import {
+  ApplicationDetailRequest,
+  ApplicationDetailResponse,
+  ApplicationsRequest,
+  ApplicationsResponse,
+  ConfirmApplicantRequest,
+  ConfirmApplicantResponse,
+  CreateMyApplicationRequest,
+  CreateMyApplicationResponse,
+  SubmitApplicationRequest,
+  SubmitApplicationResponse,
+  TempSaveApplicationRequest,
+  TempSaveApplicationResponse,
+} from '@/types/dto';
+import BaseApiService from './base';
+
+class ApplicationApiService extends BaseApiService {
+  public constructor() {
+    super('applications');
+  }
+
+  public getApplications({ accessToken }: ApplicationsRequest): Promise<ApplicationsResponse> {
+    return this.http
+      .get('/', { headers: { Authorization: `Bearer ${accessToken}` } })
+      .then(BaseApiService.handleResponse)
+      .catch(BaseApiService.handleError);
+  }
+
+  public createMyApplication({
+    accessToken,
+    teamId,
+  }: CreateMyApplicationRequest): Promise<CreateMyApplicationResponse> {
+    return this.http
+      .post('/', { teamId }, { headers: { Authorization: `Bearer ${accessToken}` } })
+      .then(BaseApiService.handleResponse)
+      .catch(BaseApiService.handleError);
+  }
+
+  public getApplicationDetail({
+    accessToken,
+    applicationId,
+  }: ApplicationDetailRequest): Promise<ApplicationDetailResponse> {
+    return this.http
+      .get(`/${applicationId}`, {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      })
+      .then(BaseApiService.handleResponse)
+      .catch(BaseApiService.handleError);
+  }
+
+  public tempSaveApplication({
+    accessToken,
+    applicationId,
+    updateApplicationRequest,
+  }: TempSaveApplicationRequest): Promise<TempSaveApplicationResponse> {
+    return this.http
+      .put(
+        `/${applicationId}`,
+        { updateApplicationRequest },
+        { headers: { Authorization: `Bearer ${accessToken}` } },
+      )
+      .then(BaseApiService.handleResponse)
+      .catch(BaseApiService.handleError);
+  }
+
+  public confirmApplicant({
+    accessToken,
+    applicationId,
+    updateConfirmationRequest,
+  }: ConfirmApplicantRequest): Promise<ConfirmApplicantResponse> {
+    return this.http
+      .post(
+        `/${applicationId}/confirm`,
+        { updateConfirmationRequest },
+        {
+          headers: { Authorization: `Bearer ${accessToken}` },
+        },
+      )
+      .then(BaseApiService.handleResponse)
+      .catch(BaseApiService.handleError);
+  }
+
+  public submitApplication({
+    accessToken,
+    applicationId,
+  }: SubmitApplicationRequest): Promise<SubmitApplicationResponse> {
+    return this.http
+      .post(
+        `/${applicationId}/submit`,
+        {},
+        {
+          headers: { Authorization: `Bearer ${accessToken}` },
+        },
+      )
+      .then(BaseApiService.handleResponse)
+      .catch(BaseApiService.handleError);
+  }
+}
+
+export default new ApplicationApiService();
