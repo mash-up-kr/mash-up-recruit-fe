65cff2e9fad4469faad6ea23b39501174307e736
HaJunRyu <hjr4557@naver.com>
2022-02-20 07:33:01 +0900

    Feat: team 관련 상수를 추가, 해당 상수를 route 상수에 적용
    Changes to be committed:
    modified:   src/constants/index.ts
    modified:   src/constants/route.ts
    new file:   src/constants/team.ts

--- src/constants/route.ts
+++ src/constants/route.ts
@@ -1,8 +1,10 @@
+import { teamUrls } from './team';
+
 export const HOME_PAGE = '/';
 export const FAQ_PAGE = '/faq';
-export const APPLY_FRONT_END_PAGE = '/apply/front-end';
-export const APPLY_DESIGN_PAGE = '/apply/design';
-export const APPLY_NODE_PAGE = '/apply/node';
-export const APPLY_SPRING_PAGE = '/apply/spring';
-export const APPLY_IOS_PAGE = '/apply/ios';
-export const APPLY_ANDROID_PAGE = '/apply/android';
+export const APPLY_FRONT_END_PAGE = `/apply/${teamUrls.web}`;
+export const APPLY_DESIGN_PAGE = `/apply/${teamUrls.design}`;
+export const APPLY_NODE_PAGE = `/apply/${teamUrls.node}`;
+export const APPLY_SPRING_PAGE = `/apply/${teamUrls.spring}`;
+export const APPLY_IOS_PAGE = `/apply/${teamUrls.ios}`;
+export const APPLY_ANDROID_PAGE = `/apply/${teamUrls.android}`;
