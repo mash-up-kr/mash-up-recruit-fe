9df72b74352371430130c7ff16381e27a6b86c26
HaJunRyu <hjr4557@naver.com>
2022-02-21 09:10:24 +0900

    Feat: font 믹스인 추가 (Spoqa Han Sans Neo bold30)
    Changes to be committed:
    modified:   src/styles/themes/fonts.ts

--- src/styles/themes/fonts.ts
+++ src/styles/themes/fonts.ts
@@ -8,6 +8,12 @@ export const fonts = {
       font-family: SpoqaHanSansNeo;
       line-height: 1.5;
     `,
+    bold30: css`
+      font-weight: 700;
+      font-size: 3rem;
+      font-family: SpoqaHanSansNeo;
+      line-height: 1.5;
+    `,
     bold24: css`
       font-weight: 700;
       font-size: 2.4rem;
