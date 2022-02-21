d485209e44430c62eff9b9efab026fadf2c5824a
Baek2back <dumbuk12@naver.com>
2022-02-22 04:08:23 +0900

    Refactor: 자주 묻는 질문 페이지 헤더 컴포넌트의 이름을 변경한다

--- src/components/faq/FaqHeader/FaqHeader.styled.ts
+++ src/components/faq/FaqHeader/FaqHeader.styled.ts
@@ -5,11 +5,16 @@ export const Heading = styled.h2`
   ${({ theme }) => css`
     ${theme.fonts.kr.bold46};
     flex-basis: 100%;
-    margin: 8.4rem 0 5.4rem;
+    margin: 8.4rem 0 5rem;
+
+    @media (max-width: ${theme.breakPoint.media.tabletL}) {
+      ${theme.fonts.kr.bold32};
+      margin: 3.6rem 0 0;
+    }
 
     @media (max-width: ${theme.breakPoint.media.tabletS}) {
       ${theme.fonts.kr.bold24};
-      margin: 5.4rem 0 3rem 0;
+      margin: 5.4rem 0 3rem;
     }
   `}
 `;
