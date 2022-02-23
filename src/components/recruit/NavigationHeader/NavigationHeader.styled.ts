925497dfb808ff6476f253551d432bc76937d398
Baek2back <dumbuk12@naver.com>
2022-02-24 03:50:19 +0900

    Feat: navigation header 컴포넌트를 구현한다

---
+++ src/components/recruit/NavigationHeader/NavigationHeader.styled.ts
@@ -0,0 +1,18 @@
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+
+export const Heading = styled.h2`
+  ${({ theme }) => css`
+    ${theme.fonts.en.extrabold46};
+    width: 100%;
+    max-width: 78.4rem;
+    margin: 0 auto 3.3rem;
+
+    @media (max-width: ${theme.breakPoint.media.tabletL}) {
+      ${theme.fonts.en.extrabold32};
+      max-width: none;
+      margin: 0 0 3.6rem;
+      padding: 0 2rem;
+    }
+  `}
+`;
