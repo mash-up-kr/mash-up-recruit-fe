f10a81a2afd23b18fa0deef87adf8a5375513b0b
Baek2back <dumbuk12@naver.com>
2022-02-25 07:34:29 +0900

    Refactor: 변경된 title 포맷에 맞게 데이터를 조정한다

--- src/constants/platform/web.tsx
+++ src/constants/platform/web.tsx
@@ -66,15 +66,15 @@ const web = {
   ),
   questions: [
     {
-      id: 'web-1',
-      title: '웹팀은 주로 어떤 활동을 하나요?',
+      id: 'web-question-1',
+      title: { text: '웹팀은 주로 어떤 활동을 하나요?' },
       content: [
         {
-          id: 'web-1-1',
+          id: 'web-question-1-1',
           content: <>전체 세미나가 없는 격주 토요일마다 플랫폼 세미나를 진행합니다 😄</>,
         },
         {
-          id: 'web-1-2',
+          id: 'web-question-1-2',
           content: (
             <>
               깜짝 모각코나 번개모임을 많이 열 예정이에요(방역수칙은 지키면서)! 누구나 열 수 있고
@@ -85,11 +85,11 @@ const web = {
       ],
     },
     {
-      id: 'web-2',
-      title: '세미나 진행은 어떻게 되나요?',
+      id: 'web-question-2',
+      title: { text: '세미나 진행은 어떻게 되나요?' },
       content: [
         {
-          id: 'web-2-1',
+          id: 'web-question-2-1',
           content: (
             <>
               간단한 근황 공유와 각자 개발하면서 쌓은 경험이나 노하우, 기술들을 함께 이야기하며
@@ -99,7 +99,7 @@ const web = {
           ),
         },
         {
-          id: 'web-2-2',
+          id: 'web-question-2-2',
           content: (
             <>
               웹 구성원들이 번갈아가며 자유로운 주제로 발표를 진행해요. 이전에는 함수형 프로그래밍,
@@ -110,11 +110,11 @@ const web = {
       ],
     },
     {
-      id: 'web-3',
-      title: '스터디 진행은 어떻게 되나요?',
+      id: 'web-question-3',
+      title: { text: '스터디 진행은 어떻게 되나요?' },
       content: [
         {
-          id: 'web-3-1',
+          id: 'web-question-3-1',
           content: (
             <>
               누구나 하고싶은 스터디를 주최할 수 있고 구성원들이 자유롭게 참여하는 형태로 진행 할
@@ -125,11 +125,11 @@ const web = {
       ],
     },
     {
-      id: 'web-4',
-      title: '리액트를 잘 몰라도 괜찮을까요?',
+      id: 'web-question-4',
+      title: { text: '리액트를 잘 몰라도 괜찮을까요?' },
       content: [
         {
-          id: 'web-4-1',
+          id: 'web-question-4-1',
           content: (
             <>
               리액트를 사용해서 작은 프로젝트라도 진행해본 경험이 있는 분들을 우선해서 뽑을
@@ -140,11 +140,11 @@ const web = {
       ],
     },
     {
-      id: 'web-5',
-      title: '웹 팀은 어떤 사람들이 모여있나요?',
+      id: 'web-question-5',
+      title: { text: '웹 팀은 어떤 사람들이 모여있나요?' },
       content: [
         {
-          id: 'web-5-1',
+          id: 'web-question-5-1',
           content: (
             <>
               웹 개발자로 일을 하고 있는 직장인도 있구요, 웹 분야를 열심히 공부하는 대학생들도
@@ -154,7 +154,7 @@ const web = {
           ),
         },
         {
-          id: 'web-5-2',
+          id: 'web-question-5-2',
           content: (
             <>
               이번에 함께할 분들도 끼가 넘치고 동아리 활동에 적극적으로 참여할 수 있는 분들을
@@ -165,17 +165,17 @@ const web = {
       ],
     },
     {
-      id: 'web-6',
-      title: '웹 팀의 목표는 무엇인가요?',
+      id: 'web-question-6',
+      title: { text: '웹 팀의 목표는 무엇인가요?' },
       content: [
         {
-          id: 'web-6-1',
+          id: 'web-question-6-1',
           content: (
             <>프로젝트를 성공적으로 배포해서 성취감을 느낄 수 있는 동아리를 만들고 싶어요!</>
           ),
         },
         {
-          id: 'web-6-2',
+          id: 'web-question-6-2',
           content: (
             <>
               혼자만 알기보단 다같이 공유하고 개발적으로 어려운 부분이 있다면 함께 해결해가고
@@ -184,7 +184,7 @@ const web = {
           ),
         },
         {
-          id: 'web-6-3',
+          id: 'web-question-6-3',
           content: (
             <>
               동아리 활동을 하면서 원하는 목표도 같이 이뤘으면 좋겠어요! 지난 기수에는 취업/이직을
@@ -193,11 +193,11 @@ const web = {
           ),
         },
         {
-          id: 'web-6-4',
+          id: 'web-question-6-4',
           content: <>재미있고 함께 성장하는 매쉬업의 구성원이 되어주세요!</>,
         },
         {
-          id: 'web-6-5',
+          id: 'web-question-6-5',
           content: <>웹 팀 뿐만 아니라 다양한 분야의 실력있는 사람들과 좋은 관계를 만들어요</>,
         },
       ],
