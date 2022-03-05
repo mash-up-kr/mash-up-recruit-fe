ad3875f4a32cb0bee7738ca5b939237062e394c6
Baek2back <dumbuk12@naver.com>
2022-03-05 23:05:21 +0900

    Feat: RecruitingOpenHero 컴포넌트를 구현한다

---
+++ src/components/home/RecruitingOpenHero/RecruitingOpenHero.component.tsx
@@ -0,0 +1,48 @@
+import Yeah from '@/assets/svg/yeah.svg';
+import DoubleUnderline from '@/assets/svg/double-underline.svg';
+import GenerationRight from '@/assets/images/generation-right-2x.png';
+import Image from 'next/image';
+import { useDetectViewPort } from '@/hooks';
+import { RecruitingPeriodDesktop } from '@/components';
+import { AOS_BASE_DURATION_DISTANCE, AOS_DEFAULT_DURATION } from '@/constants';
+import * as Styled from './RecruitingOpenHero.styled';
+
+const RecruitingOpenHero = () => {
+  const { size } = useDetectViewPort();
+  return (
+    <Styled.Container>
+      <Styled.Contents>
+        <span data-aos="fade-up" data-aos-duration={AOS_DEFAULT_DURATION}>
+          Mash-Up
+        </span>
+        <Styled.Generation>
+          <span
+            data-aos="fade-up"
+            data-aos-duration={AOS_DEFAULT_DURATION + AOS_BASE_DURATION_DISTANCE}
+          >
+            12th
+          </span>
+          <Styled.RightImageContainer>
+            <Image src={GenerationRight.src} alt="" layout="fill" />
+          </Styled.RightImageContainer>
+        </Styled.Generation>
+        <Styled.RookieRecruiting
+          data-aos="fade-up"
+          data-aos-duration={AOS_DEFAULT_DURATION + 2 * AOS_BASE_DURATION_DISTANCE}
+        >
+          Rookie Recruiting
+        </Styled.RookieRecruiting>
+        <span
+          data-aos="fade-up"
+          data-aos-duration={AOS_DEFAULT_DURATION + 3 * AOS_BASE_DURATION_DISTANCE}
+        >
+          OPEN
+        </span>
+        <DoubleUnderline />
+        <Yeah />
+        {size === 'desktop' && <RecruitingPeriodDesktop />}
+      </Styled.Contents>
+    </Styled.Container>
+  );
+};
+export default RecruitingOpenHero;
