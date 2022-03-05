d57af89dbd65ac03a197dc2013a73cea770ca2bb
HaJunRyu <hjr4557@naver.com>
2022-03-06 04:57:59 +0900

    Env: CD과정에서 yarn install과 pm2 restart단계를 명확히 구분
    Changes to be committed:
    modified:   appspec.yml
    modified:   codedeploy/after-install.sh
    new file:   codedeploy/application-start.sh

--- codedeploy/application-start.sh
+++ codedeploy/application-start.sh
@@ -4,8 +4,4 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-REPOSITORY=/home/ubuntu/deploy/mash-up-dev-recruit-fe
-
-cd $REPOSITORY
-
-yarn install
+pm2 restart recruit
