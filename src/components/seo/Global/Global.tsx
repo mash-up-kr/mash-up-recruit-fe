660e01fcca2a8d033248c5c3fe88ca300e4334f7
HaJunRyu <hjr4557@naver.com>
2022-03-10 18:43:35 +0900

    Feat: 링크 공유시 노출되는 open graph image 추가
    Changes to be committed:
    new file:   src/assets/images/og-image.png
    modified:   src/components/seo/Global/Global.tsx

--- src/components/seo/Global/Global.tsx
+++ src/components/seo/Global/Global.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import favicon from 'public/favicon.ico';
+import ogImage from '@/assets/images/ig-image.png';
 
 const GlobalSEO = () => {
   return (
@@ -25,6 +26,7 @@ const GlobalSEO = () => {
         property="og:description"
         content="매쉬업은 개발, 디자인에 관심과 열정이 있는 사람들이 모인 단체로 UX/UI Design, Android, iOS, Backend(노드,스프링), Web 총 6개의 팀으로 구성되어 있습니다. 매쉬업에서는 전체모임의 세미나 및 네트워킹을 진행하고 있으며, 이를 통하여 개인의 전문역량과 협업능력을 증대시키고자 합니다."
       />
+      <meta property="og:image" content={ogImage.src} />
       <link rel="icon" href={favicon.src} />
     </Head>
   );
