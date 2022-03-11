84b80f4cdfae43cbaa4121c8245e34eaa5a1badd
HaJunRyu <hjr4557@naver.com>
2022-03-11 23:49:56 +0900

    Feat: real 배포 브랜치인 main에 CI/CD 추가
    Changes to be committed:
    new file:   .github/workflows/main-deploy.yml
    new file:   appspec.yml
    new file:   codedeploy/after-install.sh
    new file:   codedeploy/application-start.sh

---
+++ codedeploy/after-install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+REPOSITORY=/home/ubuntu/deploy/mash-up-recruit-fe
+
+cd $REPOSITORY
+
+yarn install
