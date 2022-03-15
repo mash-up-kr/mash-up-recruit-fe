9bf1c88285546bf7987d0bb1995d2a65d36521ac
Baek2back <dumbuk12@naver.com>
2022-03-15 15:41:13 +0900

    Feat: 환경변수 타입 선언을 추가한다

--- @types/custom-types/process.d.ts
+++ @types/custom-types/process.d.ts
@@ -1,10 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 declare namespace NodeJS {
   export interface ProcessEnv {
+    BASE_URL: string;
     NEXTAUTH_URL: string;
     NEXTAUTH_SECRET: string;
     GOOGLE_ID: string;
     GOOGLE_SECRET: string;
+    NEXT_PUBLIC_GOOGLE_ANALYTICS: string;
+    NEXT_PUBLIC_CHANNEL_PLUGIN: string;
     DESTINATION_URL: string;
     SOURCE_PATH: string;
   }
