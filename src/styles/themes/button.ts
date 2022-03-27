61926336bf322d6c9065314cabfd459924a47be9
HaJunRyu <hjr4557@naver.com>
2022-03-28 03:09:50 +0900

    Feat: button mixin 추가
    Changes to be committed:
    modified:   src/styles/themes/button.ts

--- src/styles/themes/button.ts
+++ src/styles/themes/button.ts
@@ -80,6 +80,76 @@ export const button = {
         border-color: ${colors.gray30};
       }
     `,
+    white: css`
+      ${fonts.kr.medium16};
+      height: 5.6rem;
+      padding: 1.6rem 1.95rem;
+      color: ${colors.gray70};
+      background: ${colors.white};
+      border: 0.1rem solid transparent;
+      border-radius: 1.6rem;
+      transition: 0.3s;
+
+      @media (hover: hover) {
+        &:hover {
+          background: ${colors.gray40};
+        }
+      }
+
+      &:active {
+        background: ${colors.gray40};
+      }
+    `,
+    gray20: css`
+      ${fonts.kr.medium16};
+      height: 5.6rem;
+      padding: 1.6rem 1.95rem;
+      color: ${colors.gray80};
+      background: ${colors.gray20};
+      border: 0.1rem solid transparent;
+      border-radius: 1.6rem;
+      transition: 0.3s;
+
+      @media (hover: hover) {
+        &:hover {
+          background: ${colors.gray40};
+        }
+      }
+
+      &:active {
+        background: ${colors.gray40};
+      }
+
+      &:disabled {
+        color: ${colors.gray40};
+        background: ${colors.gray20};
+      }
+    `,
+    gray40: css`
+      ${fonts.kr.medium16};
+      height: 5.6rem;
+      padding: 1.6rem 1.95rem;
+      color: ${colors.gray80};
+      background: ${colors.gray40};
+      border: 0.1rem solid transparent;
+      border-radius: 1.6rem;
+      transition: 0.3s;
+
+      @media (hover: hover) {
+        &:hover {
+          background: ${colors.gray60};
+        }
+      }
+
+      &:active {
+        background: ${colors.gray60};
+      }
+
+      &:disabled {
+        color: ${colors.gray50};
+        background: ${colors.gray40};
+      }
+    `,
   },
 };
 
