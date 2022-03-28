2c7786cb83aebe11de49ca72a460bb0de463db3c
Baek2back <dumbuk12@naver.com>
2022-03-29 01:06:51 +0900

    Feat: 서류 마감 기한에 대한 질문을 추가한다

--- pages/faq/[platformName].tsx
+++ pages/faq/[platformName].tsx
@@ -14,10 +14,24 @@ const platformWithCommonMap = {
     questions: [
       {
         id: 'common-question-1',
-        title: { text: '매쉬업은 언제 모임을 가지나요?' },
+        title: { text: '지원 마감시간은 언제인가요?' },
         content: [
           {
             id: 'common-question-1-1',
+            content: (
+              <>
+                3월 29일(화) 오후 11시 59분 59초입니다. 마감 시간 안에 지원서 제출해주시길 바랍니다.
+              </>
+            ),
+          },
+        ],
+      },
+      {
+        id: 'common-question-2',
+        title: { text: '매쉬업은 언제 모임을 가지나요?' },
+        content: [
+          {
+            id: 'common-question-2-1',
             content: (
               <>
                 Mash-Up은 격주 토요일 오후 2시에 전체 세미나를 진행하며, 매주 평일 혹은 주말에 팀별
@@ -28,11 +42,11 @@ const platformWithCommonMap = {
         ],
       },
       {
-        id: 'common-question-2',
+        id: 'common-question-3',
         title: { text: '코시국 매쉬업 모임은 어떻게 진행되나요?' },
         content: [
           {
-            id: 'common-question-2-1',
+            id: 'common-question-3-1',
             content: (
               <>
                 코시국 매쉬업은 Zoom, Discord 채널, 그리고 Google Meet을 통해 매쉬업 전체 세미나 및
@@ -42,7 +56,7 @@ const platformWithCommonMap = {
             ),
           },
           {
-            id: 'common-question-2-2',
+            id: 'common-question-3-2',
             content: (
               <>
                 12기는 거리두기 정책의 변화에 맞춰 활동을 하고자 하며, 인원 제한에 맞춰 오프라인
@@ -53,11 +67,11 @@ const platformWithCommonMap = {
         ],
       },
       {
-        id: 'common-question-3',
+        id: 'common-question-4',
         title: { text: '활동 기간은 얼마나 되나요?' },
         content: [
           {
-            id: 'common-question-3-1',
+            id: 'common-question-4-1',
             content: (
               <>
                 12기의 경우 4월 16일을 시작으로 10월까지 약 6개월 정도 활동할 예정입니다. 2기수
@@ -68,21 +82,21 @@ const platformWithCommonMap = {
         ],
       },
       {
-        id: 'common-question-4',
+        id: 'common-question-5',
         title: { text: '직장인 또는 대학생, 졸업생인데 참여 가능한가요?' },
         content: [
           {
-            id: 'common-question-4-1',
+            id: 'common-question-5-1',
             content: <>직장인은 물론 대학생, 졸업생 누구나 참여 가능합니다.</>,
           },
         ],
       },
       {
-        id: 'common-question-5',
+        id: 'common-question-6',
         title: { text: '매쉬업에 가면 무엇을 하나요?' },
         content: [
           {
-            id: 'common-question-5-1',
+            id: 'common-question-6-1',
             content: (
               <>
                 매쉬업은 팀별 스터디와 프로젝트를 진행하고 있습니다. 각 팀별로 스터디를 진행하고,
@@ -95,11 +109,11 @@ const platformWithCommonMap = {
         ],
       },
       {
-        id: 'common-question-6',
+        id: 'common-question-7',
         title: { text: '회비 있나요?' },
         content: [
           {
-            id: 'common-question-6-1',
+            id: 'common-question-7-1',
             content: (
               <>
                 네. 비용은 6만원으로 2개월 분할납부 가능합니다. 온라인 행사 참여 상품 및 해커톤을
@@ -110,11 +124,11 @@ const platformWithCommonMap = {
         ],
       },
       {
-        id: 'common-question-7',
+        id: 'common-question-8',
         title: { text: '중복지원이 가능한가요?' },
         content: [
           {
-            id: 'common-question-7-1',
+            id: 'common-question-8-1',
             content: (
               <>
                 중복지원은 불가합니다. 팀별 지원서 임시저장은 가능하나, 최종 제출한 단 한 개의
