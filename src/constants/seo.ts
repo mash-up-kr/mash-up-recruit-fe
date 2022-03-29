c3a189a90ae1bb9b3e7438433f5b50ef4ee714ef
Baek2back <dumbuk12@naver.com>
2022-03-29 10:01:13 +0900

    Feat: og-image 대체 텍스트를 수정한다

--- src/constants/seo.ts
+++ src/constants/seo.ts
@@ -11,7 +11,7 @@ export const seoDefaultConfig: DefaultSEOProps = {
     url: 'https://recruit.mash-up.kr',
     description:
       '매쉬업은 개발, 디자인에 관심과 열정이 있는 사람들이 모인 단체로 Product Design, Android, iOS, Backend(노드,스프링), Web 총 6개의 팀으로 구성되어 있습니다. 매쉬업에서는 전체모임의 세미나 및 네트워킹을 진행하고 있으며, 이를 통하여 개인의 전문역량과 협업능력을 증대시키고자 합니다.',
-    images: [{ url: ogImage.src, alt: 'Mash Up Open Graph 이미지' }],
+    images: [{ url: ogImage.src, alt: 'Mash Up Recruit' }],
   },
   defaultOpenGraphImageWidth: 800,
   defaultOpenGraphImageHeight: 400,
