5646c3283ce895f318a4dbac11869e4adfd01de9
HaJunRyu <hjr4557@naver.com>
2022-03-30 18:10:32 +0900

    Refactor: 서류 결과 발표날짜 변경
    Changes to be committed:
    modified:   src/components/applyStatus/ScreeningWait/ScreeningWait.component.tsx
    modified:   src/utils/date.ts

--- src/components/applyStatus/ScreeningWait/ScreeningWait.component.tsx
+++ src/components/applyStatus/ScreeningWait/ScreeningWait.component.tsx
@@ -8,7 +8,7 @@ const ScreeningWait = () => {
         {'Mash-Up Staff는 열심히 서류 검토 중.. \n조금만 기다려주세요!'}
       </Styled.StatusMessage>
       <Styled.StatusDescription>
-        Mash-Up Staff가 꼼꼼히 서류를 검토하고 있습니다. 4월 2일(토)에 서류 결과 발표를 꼭
+        Mash-Up Staff가 꼼꼼히 서류를 검토하고 있습니다. 4월 3일(일) 오전 10시에 서류 결과 발표를 꼭
         확인해주세요!
       </Styled.StatusDescription>
     </Styled.ScreeningWait>
