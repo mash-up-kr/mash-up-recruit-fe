d013304078a4233cb53a36f7c5ac52458e49d0f8
HaJunRyu <hjr4557@naver.com>
2022-04-12 03:09:22 +0900

    Refactor: 한글 폰트에 기본 사용되는 letter-spacing속성 믹스인에 추가
    Changes to be committed:
    modified:   src/styles/themes/fonts.ts

--- src/styles/themes/fonts.ts
+++ src/styles/themes/fonts.ts
@@ -7,102 +7,119 @@ export const fonts = {
       font-size: 4.6rem;
       font-family: SpoqaHanSansNeo;
       line-height: 1.5;
+      letter-spacing: -0.08rem;
     `,
     bold32: css`
       font-weight: 700;
       font-size: 3.2rem;
       font-family: SpoqaHanSansNeo;
       line-height: 1.5;
+      letter-spacing: -0.08rem;
     `,
     bold30: css`
       font-weight: 700;
       font-size: 3rem;
       font-family: SpoqaHanSansNeo;
       line-height: 1.5;
+      letter-spacing: -0.08rem;
     `,
     bold26: css`
       font-weight: 700;
       font-size: 2.6rem;
       font-family: SpoqaHanSansNeo;
       line-height: 1.5;
+      letter-spacing: -0.08rem;
     `,
     bold24: css`
       font-weight: 700;
       font-size: 2.4rem;
       font-family: SpoqaHanSansNeo;
       line-height: 1.5;
+      letter-spacing: -0.08rem;
     `,
     bold22: css`
       font-weight: 700;
       font-size: 2.2rem;
       font-family: SpoqaHanSansNeo;
       line-height: 1.5;
+      letter-spacing: -0.08rem;
     `,
     bold20: css`
       font-weight: 700;
       font-size: 2rem;
       font-family: SpoqaHanSansNeo;
       line-height: 1.5;
+      letter-spacing: -0.08rem;
     `,
     bold18: css`
       font-weight: 700;
       font-size: 1.8rem;
       font-family: SpoqaHanSansNeo;
       line-height: 1.5;
+      letter-spacing: -0.08rem;
     `,
     bold16: css`
       font-weight: 700;
       font-size: 1.6rem;
       font-family: SpoqaHanSansNeo;
       line-height: 1.5;
+      letter-spacing: -0.08rem;
     `,
     medium16: css`
       font-weight: 500;
       font-size: 1.6rem;
       font-family: SpoqaHanSansNeo;
       line-height: 1.5;
+      letter-spacing: -0.08rem;
     `,
     medium15: css`
       font-weight: 500;
       font-size: 1.5rem;
       font-family: SpoqaHanSansNeo;
       line-height: 1.5;
+      letter-spacing: -0.08rem;
     `,
     medium14: css`
       font-weight: 500;
       font-size: 1.4rem;
       font-family: SpoqaHanSansNeo;
       line-height: 1.5;
+      letter-spacing: -0.08rem;
     `,
     regular16: css`
       font-weight: 400;
       font-size: 1.6rem;
       font-family: SpoqaHanSansNeo;
       line-height: 1.5;
+      letter-spacing: -0.08rem;
     `,
     regular15: css`
       font-weight: 400;
       font-size: 1.5rem;
       font-family: SpoqaHanSansNeo;
       line-height: 1.5;
+      letter-spacing: -0.08rem;
     `,
     regular14: css`
       font-weight: 400;
       font-size: 1.4rem;
       font-family: SpoqaHanSansNeo;
       line-height: 1.5;
+      letter-spacing: -0.08rem;
     `,
     medium13: css`
       font-weight: 500;
       font-size: 1.3rem;
       font-family: SpoqaHanSansNeo;
       line-height: 1.5;
+      letter-spacing: -0.08rem;
     `,
     regular13: css`
       font-weight: 400;
       font-size: 1.3rem;
       font-family: SpoqaHanSansNeo;
       line-height: 1.5;
+      letter-spacing: -0.08rem;
     `,
   },
   en: {
