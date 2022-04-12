466b3f3f7f5c37c64730f7fb152bac2ffb85919c
HaJunRyu <hjr4557@naver.com>
2022-04-12 18:18:11 +0900

    Refactor: 인터뷰 설문 링크 위치 변경 및 스타일 변경
    Changes to be committed:
    modified:   src/components/applyStatus/ProcessFail/ProcessFail.component.tsx
    modified:   src/components/applyStatus/ProcessFail/ProcessFail.styled.ts

--- src/components/applyStatus/ProcessFail/ProcessFail.component.tsx
+++ src/components/applyStatus/ProcessFail/ProcessFail.component.tsx
@@ -13,8 +13,6 @@ const ProcessFail = ({ heading, paragraph, survey = false }: ProcessFailProps) =
       <Styled.FailParagraph>{paragraph}</Styled.FailParagraph>
       {survey && (
         <Styled.SurveyRequestMessage>
-          Mash-Up 12기 모집 프로세스 경험에 대한 만족도 설문을 진행하고 있습니다. 선택사항이며,
-          지원자님의 소중한 답변을 통해 더 나은 프로세스를 만들어 성장하는 Mash-Up이 되겠습니다.
           <Styled.SatisfactionSurveyLink
             href="https://forms.gle/36cdamKfsDFwqLUg7"
             target="_blank"
@@ -22,6 +20,8 @@ const ProcessFail = ({ heading, paragraph, survey = false }: ProcessFailProps) =
           >
             https://forms.gle/36cdamKfsDFwqLUg7
           </Styled.SatisfactionSurveyLink>
+          Mash-Up 12기 모집 프로세스 경험에 대한 만족도 설문을 진행하고 있습니다. 선택사항이며,
+          지원자님의 소중한 답변을 통해 더 나은 프로세스를 만들어 성장하는 Mash-Up이 되겠습니다.
         </Styled.SurveyRequestMessage>
       )}
     </Styled.ProcessFail>
