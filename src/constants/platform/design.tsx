2ef5d6ba3a0d844f742e5ed6db7f385df4e6bd4e
HaJunRyu <hjr4557@naver.com>
2022-06-18 13:30:21 +0900

    Feat: aria-labelledby를 위한 id 추가

--- src/constants/platform/design.tsx
+++ src/constants/platform/design.tsx
@@ -3,6 +3,7 @@ import DesignLeftEmoji from '@/assets/svg/design-left-emoji.svg';
 import DesignRightEmoji from '@/assets/svg/design-right-emoji.svg';
 
 const design = {
+  id: 'productDesign',
   name: 'Product Design',
   role: 'Product Designer',
   path: {
