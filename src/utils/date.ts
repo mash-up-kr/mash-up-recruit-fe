fe4729c083c449bdd8d0add7060e83a46290a3aa
HaJunRyu <hjr4557@naver.com>
2023-01-09 21:55:01 +0900

    Test: QA를 위한 모집 시간값을 수정한다

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -10,10 +10,10 @@ export const [
   INTERVIEW_RESULT_ANNOUNCED_KST_DATE, // 최종 합격 발표
   AFTER_FIRST_SEMINAR_JOIN_KST_DATE, // 첫번째 세미나 끝나는 시각
 ] = [
-  new Date('2023-01-11T00:00:00+09:00'),
-  new Date('2023-01-25T23:59:59+09:00'),
-  new Date('2023-01-30T21:00:00+09:00'),
-  new Date('2023-02-07T21:00:00+09:00'),
+  new Date('2023-01-09T22:10:00+09:00'),
+  new Date('2023-01-09T22:20:00+00:00'),
+  new Date('2023-01-09T22:25:00+09:00'),
+  new Date('2023-01-09T22:35:00+09:00'),
   new Date('2023-02-11T17:00:00+09:00'),
 ];
 
