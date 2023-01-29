793e1be3b18780c0b2e115861ec040ae1e45cc8e
HaJunRyu <hjr4557@naver.com>
2023-01-29 17:41:09 +0900

    Refactor: 면접 불합격시 노출 될 문구를 수정한다

--- src/components/applyStatus/InterviewFail/InterviewFail.component.tsx
+++ src/components/applyStatus/InterviewFail/InterviewFail.component.tsx
@@ -16,14 +16,7 @@ const InterviewFail = ({ application }: InterviewFailProps) => {
         heading={`Mash-Up ${CURRENT_GENERATION}기 ${
           size === 'mobile' ? '\n' : ''
         }Rookie Recruiting \n${TEAM_NICK_NAME[team.name]} 면접 결과 안내`}
-        paragraph={`안녕하세요, ${
-          applicant.name
-        }님! Mash-Up입니다. 먼저 Mash-Up ${CURRENT_GENERATION}기에 많은 관심 가져 주시고, 귀한 시간 내어 지원해 주셔서 감사합니다. 보내주신 지원서와 면접 경험을 바탕으로 면밀히 살펴보고 ${
-          TEAM_NICK_NAME[team.name]
-        }이 추구하는 인재상과 잘 어울리는지 평가하였습니다.
-  아쉽지만 ${applicant.name}님의 뛰어난 역량에도 불구하고, 제한된 모집 인원으로 인해 ${
-          applicant.name
-        }님을 Mash-Up ${CURRENT_GENERATION}기에 모실 수 없게 되었습니다. Mash-Up ${CURRENT_GENERATION}기 Rookie Recruiting에 지원해주셔서 다시 한 번 감사드리며, 다음 기회에 인연이 될 수 있으면 좋겠습니다. 감사합니다.`}
+        paragraph={`안녕하세요, ${applicant.name}님 Mash-Up에 관심을 갖고 지원해주셔서 진심으로 감사드립니다. ${applicant.name}님과 Mash-Up이 함께 하는 방향을 진지하게 숙고했지만 아쉽게도 이번 Mash-Up ${CURRENT_GENERATION}기 면접 전형에서는 함께 하기 어렵다는 말씀을 전하게 되었습니다. Mash-Up ${CURRENT_GENERATION}기에 함께 하기 위해 ${applicant.name}님께서 보여주신 열정과 시간을 생각하면 함께 하지 못한다는 사실을 말씀드리기 매우 조심스럽고 죄송스러운 마음입니다. 지금 당장은 인연이 닿지 않았지만 추후 더 좋은 기회에 꼭 다시 만나뵐 수 있도록 저희도 노력하겠습니다. 다시 한번 Mash-Up ${CURRENT_GENERATION}기에 보여주신 관심과 열정에 깊은 감사를 드립니다.`}
         survey
       />
     </StatusDetailBackground>
