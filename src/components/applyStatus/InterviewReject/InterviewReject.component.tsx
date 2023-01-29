7bf9b288347e5696fd5151ba825eb0bd9a22a8b3
HaJunRyu <hjr4557@naver.com>
2023-01-29 17:45:29 +0900

    Refactor: 결과 문구에 들어가는 기수 정보를 상수로 사용한다

--- src/components/applyStatus/InterviewReject/InterviewReject.component.tsx
+++ src/components/applyStatus/InterviewReject/InterviewReject.component.tsx
@@ -1,5 +1,6 @@
 import { Application } from '@/types/dto';
 import { ProcessFail, StatusDetailBackground } from '@/components';
+import { CURRENT_GENERATION } from '@/constants';
 
 interface InterviewRejectProps {
   application: Application;
@@ -10,8 +11,8 @@ const InterviewReject = ({ application }: InterviewRejectProps) => {
   return (
     <StatusDetailBackground imageType="basic" contentSize="s">
       <ProcessFail
-        heading="Mash-Up 13기 면접을 거절하셨습니다."
-        paragraph={`안녕하세요 ${applicant.name}님, Mash-Up에 관심을 갖고 지원해주셔서 진심으로 감사드립니다. 지금 당장은 인연이 닿지 않았지만 추후 더 좋은 기회에 꼭 다시 만나뵐 수 있도록 저희도 노력하겠습니다. 다시 한번 Mash-Up 13기에 보여주신 관심과 열정에 깊은 감사를 드립니다. `}
+        heading={`Mash-Up ${CURRENT_GENERATION}기 면접을 거절하셨습니다.`}
+        paragraph={`안녕하세요 ${applicant.name}님, Mash-Up에 관심을 갖고 지원해주셔서 진심으로 감사드립니다. 지금 당장은 인연이 닿지 않았지만 추후 더 좋은 기회에 꼭 다시 만나뵐 수 있도록 저희도 노력하겠습니다. 다시 한번 Mash-Up ${CURRENT_GENERATION}기에 보여주신 관심과 열정에 깊은 감사를 드립니다.`}
       />
     </StatusDetailBackground>
   );
--- src/components/applyStatus/ScreeningFail/ScreeningFail.component.tsx
+++ src/components/applyStatus/ScreeningFail/ScreeningFail.component.tsx
@@ -16,7 +16,7 @@ const ScreeningFail = ({ application }: ScreeningFailProps) => {
         heading={`Mash-Up ${CURRENT_GENERATION}기 ${
           size === 'mobile' ? '\n' : ''
         }Rookie Recruiting \n${TEAM_NICK_NAME[team.name]} 서류 결과 안내`}
-        paragraph={`안녕하세요 ${applicant.name}님, Mash-Up에 관심을 갖고 지원해주셔서 진심으로 감사드립니다. ${applicant.name}님과 Mash-Up이 함께 하는 방향을 진지하게 숙고했지만 아쉽게도 이번 Mash-Up 13기 1차 서류 전형에서는 함께 하기 어렵다는 말씀을 전하게 되었습니다. Mash-Up 13기에 함께 하기 위해 ${applicant.name}님께서 보여주신 열정과 시간을 생각하면 함께 하지 못한다는 사실을 말씀드리기 매우 조심스럽고 죄송스러운 마음입니다. 지금 당장은 인연이 닿지 않았지만 추후 더 좋은 기회에 꼭 다시 만나뵐 수 있도록 저희도 노력하겠습니다. 다시 한번 Mash-Up 13기에 보여주신 관심과 열정에 깊은 감사를 드립니다.`}
+        paragraph={`안녕하세요 ${applicant.name}님, Mash-Up에 관심을 갖고 지원해주셔서 진심으로 감사드립니다. ${applicant.name}님과 Mash-Up이 함께 하는 방향을 진지하게 숙고했지만 아쉽게도 이번 Mash-Up ${CURRENT_GENERATION}기 1차 서류 전형에서는 함께 하기 어렵다는 말씀을 전하게 되었습니다. Mash-Up ${CURRENT_GENERATION}기에 함께 하기 위해 ${applicant.name}님께서 보여주신 열정과 시간을 생각하면 함께 하지 못한다는 사실을 말씀드리기 매우 조심스럽고 죄송스러운 마음입니다. 지금 당장은 인연이 닿지 않았지만 추후 더 좋은 기회에 꼭 다시 만나뵐 수 있도록 저희도 노력하겠습니다. 다시 한번 Mash-Up ${CURRENT_GENERATION}기에 보여주신 관심과 열정에 깊은 감사를 드립니다.`}
       />
     </StatusDetailBackground>
   );
