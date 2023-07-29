6b74359feafe3c750c8fcc425979dd86222bacb0
Baek2back <dumbuk12@naver.com>
2023-07-30 04:51:30 +0900

    Feat: editorjs 데이터를 질문 형식에 맞게 변환한다

---
+++ src/utils/faq/transformer.ts
@@ -0,0 +1,44 @@
+import editorjsHTML from 'editorjs-html';
+
+type ListItem = {
+  content: string;
+  items: ListItem[];
+};
+
+export type Block = {
+  type: 'header' | 'list' | 'delimiter';
+  data: {
+    text?: string;
+    level?: number;
+    items?: string[] | ListItem[];
+  };
+};
+
+export type FaqQuestion = {
+  title: string;
+  content: string;
+};
+
+const parser = editorjsHTML();
+
+const transformer = ({ blocks }: { blocks: Block[] }): FaqQuestion[] => {
+  return blocks
+    .reduce((acc, block) => {
+      if (block.type === 'header') {
+        return [...acc, [block]];
+      }
+      if (block.type === 'list') {
+        return [...acc.slice(0, acc.length - 1), [...acc[acc.length - 1], block]];
+      }
+      return acc;
+    }, [] as Block[][])
+    .map(([headerBlock, listBlock]) => {
+      return [
+        ['title', headerBlock.data.text],
+        ['content', parser.parseBlock(listBlock)],
+      ];
+    })
+    .map(Object.fromEntries) as FaqQuestion[];
+};
+
+export default transformer;
